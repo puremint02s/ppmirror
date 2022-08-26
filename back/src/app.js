f952b459bb368b99589038c4f984dc254172884c
robinyeon <robin.yeon@gmail.com>
2022-08-26 09:26:24 +0900

    업데이트: app.js 깃 오류용

--- back/src/app.js
+++ back/src/app.js
@@ -25,7 +25,6 @@ app.get("/", (req, res) => {
 app.use(userAuthRouter);
 app.use(projectRouter);
 app.use(certificateRouter);
-
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
 
