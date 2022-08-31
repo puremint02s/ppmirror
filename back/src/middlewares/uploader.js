54546b6031c9fdeefe4a226d9eb6e239f49bb36e
[AI5/1반] 이태의 <wishty@gmail.com>
2022-09-01 08:28:00 +0900

    업로더 코드 정리

--- back/src/middlewares/uploader.js
+++ back/src/middlewares/uploader.js
@@ -1,14 +1,12 @@
 import multer from "multer";
 import * as path from "path";
 
-
 const storage = multer.diskStorage({
   destination : function (req, file, cb) {
     cb(null, 'public/images')
   },
   filename : function (req, file, cb) {
     const ext = path.extname(file.originalname);
-    console.log(req.params.id);
     cb(null, req.params.id + ext);
   }
 })
@@ -16,7 +14,6 @@ const storage = multer.diskStorage({
 export const uploader = multer({
   storage: storage,
   fileFilter: function (req, file, callback) {
-    // const ext = path.extname(Buffer.from(file.originalname + '-' + Date.now(), 'latin1').toString('utf8'));
     const ext = path.extname(file.originalname);
     if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
       return callback(new Error('PNG, JPG만 업로드하세요'))
@@ -27,4 +24,3 @@ export const uploader = multer({
     fileSize: 1024 * 1024 * 10
   }
 });
-
