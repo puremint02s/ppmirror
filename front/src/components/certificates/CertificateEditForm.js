e5010e4a68f1aa73c1354917fe9f37888cf5906c
jk <puremint02s@naver.com>
2022-08-31 10:37:35 +0900

    certificate get을 put으로 변경

--- front/src/components/certificates/CertificateEditForm.js
+++ front/src/components/certificates/CertificateEditForm.js
@@ -4,38 +4,49 @@ import * as Api from "../../api";
 import DatePicker from "react-datepicker";
 
 function CertificateEditForm({ currentCertificate, getCertificates,setIsEditing }) {
-  //useState로 name 상태를 생성함.
-  const [ title, setTitle] = useState(currentCertificate.title);
-//useState로 description 상태를 생성함.
-  const [description, setDescription] = useState(currentCertificate.description);
+  const [form, setForm] = useState({
+    title: currentCertificate.title,
+    description: currentCertificate.description,
+    certificateId:currentCertificate.certificateId
+  });
   const [AcquiredAt, setAcquiredAt] = useState(new Date(currentCertificate.acquiredAt));
+
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const userId = currentCertificate.userId;
+   
     const acquiredAt = AcquiredAt.toISOString().split("T")[0];
-    const certificateId=currentCertificate.certificateId
-    // "users/유저id" 엔드포인트로 PUT 요청함.
-    // await Api.put(`certificates/${currentCertificate.certificateId}`, {
-    //   certificateId,
-    //   title,
-    //   description,
-    //   acquiredAt,
-    // });
-    // // 유저 정보는 response의 data임.d
-    // const res = await Api.get("certificates", userId);
-    // setCertificates(res.data);
-    // setIsEditing(false);
+    
+
 
     try {
-     await Api.put(`certificates/${currentCertificate.certificateId}`, {
-      certificateId,
-      title,
-      description,
-      acquiredAt,
+      const id = currentCertificate.certificateId;
+
+     await Api.put(`certificates/${id}`, {
+      ...form,
+      acquiredAt
+
     })
-      const res = await Api.get("certificates", userId);
-    getCertificates(res.data);
+
+    
+    const cert = {
+      certificateId: id,
+      ...form,
+      acquiredAt
+    };
+
+    await getCertificates((prev) =>
+    prev.map((el) => (el.certificateId === cert.certificateId ? cert : el))
+  );
+
     setIsEditing(false);
     } catch (e) {
       console.log(e);
@@ -45,13 +56,14 @@ function CertificateEditForm({ currentCertificate, getCertificates,setIsEditing 
   return (
     <Card className="mb-2">
       <Card.Body>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} key={currentCertificate.certificateId}>
           <Form.Group controlId="certificateEditTitle" className="mb-3">
             <Form.Control
               type="text"
               placeholder="자격증 제목"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              name="title"
+              value={form.title}
+              onChange={handleChange}
             />
           </Form.Group>
 
@@ -59,8 +71,9 @@ function CertificateEditForm({ currentCertificate, getCertificates,setIsEditing 
             <Form.Control
               type="text"
               placeholder="상세내용"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
+              name="description"
+              value={form.description}
+              onChange={handleChange}
             />
           </Form.Group>
 
