a9a6b852b775bd53665fe922c1f049f5f31a0c86
Ryn <ryn1501@gmail.com>
2022-09-01 14:05:06 +0900

    award 주석 정리

--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -35,7 +35,7 @@ function AwardAddForm({ portfolioOwnerId, setAward, setIsAdding }) {
       setAward(res.data);
       setIsAdding(false);
     } catch (e) {
-      console.log("추가 실패", e);
+      console.log(e);
     }
   }
 
