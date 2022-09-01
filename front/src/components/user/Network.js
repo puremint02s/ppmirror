b3e47069bd4dc70e972d064fe5d9d85f2f82384c
jk <puremint02s@naver.com>
2022-09-01 18:36:55 +0900

    충돌해결

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -4,7 +4,7 @@ import { Container, Row } from "react-bootstrap";
 import * as Api from "../../api";
 import UserCard from "./UserCard";
 import { UserStateContext } from "../../App";
-
+import Modal from "../popup/modal";
 import Pagination from "./Pagination";
 
 function Network() {
@@ -14,8 +14,16 @@ function Network() {
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(10);
   const [totalUser, setTotalUser] = useState(20);
+  const [modalOpen, setModalOpen] = useState(true);
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   useEffect(() => {
+    // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
     if (!userState.user) {
       navigate("/login");
       return;
@@ -35,13 +43,13 @@ function Network() {
   }, [userState, navigate, page]);
 
   return (
-<<<<<<< HEAD
     <>
       <Modal
         open={modalOpen}
         close={closeModal}
         header="베스트 포트폴리오"
       ></Modal>
+
       <Container>
         <Row xs="auto" className="jusify-content-center">
           {users.map((user) => (
@@ -56,21 +64,6 @@ function Network() {
         />
       </Container>
     </>
-=======
-    <Container>
-      <Row xs="auto" className="jusify-content-center">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} isNetwork />
-        ))}
-      </Row>
-      <Pagination
-        total={totalUser}
-        perPage={perPage}
-        page={page}
-        setPage={setPage}
-      />
-    </Container>
->>>>>>> 8ca5873d14317b36a1a12a9c9dbc3c43c9cfa102
   );
 }
 
