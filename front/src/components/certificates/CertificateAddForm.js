60ca027e3b488e99b5a8746854783140a9c5cc9a
jk <puremint02s@naver.com>
2022-09-02 13:06:20 +0900

    certificate add 유효성체크

--- front/src/components/certificates/CertificateAddForm.js
+++ front/src/components/certificates/CertificateAddForm.js
@@ -2,47 +2,73 @@ import React, { useState } from "react";
 import { Button, Form, Card, Col, Row } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import * as Api from "../../api";
+import * as Util from "../../util";
 
 function CertificateAddForm({
   portfolioOwnerId,
   setIsAdding,
   getCertificates,
 }) {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+  });
+
   const [AcquiredAt, setAcquiredAt] = useState(new Date());
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     e.stopPropagation();
+    if (!Util.handleCheck(form)) {
+      return false;
+    }
+
     const userId = portfolioOwnerId;
     const acquiredAt = AcquiredAt.toISOString().split("T")[0];
-    await Api.post("certificate/create", {
-      title,
-      description,
-      acquiredAt,
-    });
-    const res = await Api.get("certificates", userId);
-    getCertificates(res.data);
-    setIsAdding(false);
+
+    try {
+      await Api.post("certificate/create", {
+        userId,
+        ...form,
+        acquiredAt
+      });
+
+      const res = await Api.get("certificates", userId);
+      getCertificates(res.data);
+      setIsAdding(false);
+    } catch (e) {
+      console.log(e);
+    }
   };
+
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group controlId="formBasicTitle" className="mb-3">
         <Form.Control
           type="text"
           placeholder="자격증 제목"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          name="title"
+          value={form.title}
+          onChange={handleChange}
         />
       </Form.Group>
       <Form.Group controlId="formBasicDescription" className="mb-3">
         <Form.Control
           type="text"
           placeholder="상세내용"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          name="description"
+          value={form.description}
+          onChange={handleChange}
         />
       </Form.Group>
+
       <Form.Group as={Row} className="mt-3">
         <Col xs="auto">
           <DatePicker
